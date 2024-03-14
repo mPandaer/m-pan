7c50d15ad8d5bc3e193a3231305847400b7ed3f4
mPandaer <liwenhaolx@gmail.com>
2024-03-14 14:57:19 +0800

    代码结构优化

---
+++ framework/lock/lock-core/src/main/java/com/pandaer/pan/lock/core/LockConstants.java
@@ -0,0 +1,15 @@
+package com.pandaer.pan.lock.core;
+
+
+/**
+ * 锁相关的公用常量
+ */
+public interface LockConstants {
+
+    String M_PAN_LOCK = "m-pan-lock";
+
+    /**
+     * 主要针对zk这样的节点型软件
+     */
+    String M_PAN_LOCK_PATH = "/m-pan-lock";
+}
